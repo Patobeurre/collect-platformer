9acbb1d4681d9dfe456c8e1f4709b7c1d406a011
Patobeurre <brice@zaclys.net>
2025-10-23 15:25:35 +0200

    respawnable resources

--- models/breakables/BreakableObject.gd
+++ models/breakables/BreakableObject.gd
@@ -2,10 +2,15 @@ extends Node3D
 class_name BreakableObject
 
 @export var res: BreakableObjRes = null
-@export var anim_name: String = "squash_stretch"
+@export var anim_squash: String = "squash_stretch"
+@export var anim_appear: String = "appear"
 
 @onready var mesh_node :Node3D = $Mesh
 @onready var anim_player :AnimationPlayer = $AnimationPlayer
+
+@onready var timer :Timer = $RespawnCD
+
+var collider :Node3D = null
 
 
 func _ready() -> void:
@@ -20,29 +25,52 @@ func init(init_res :BreakableObjRes) -> void:
 func clearMesh() -> void:
 	for child in mesh_node.get_children():
 		child.queue_free()
+	
+	if collider != null:
+		collider.queue_free()
 
 
 func loadRes(res_to_load :BreakableObjRes) -> void:
 	res = res_to_load.duplicate()
 	anim_player.stop()
 	
+	instantiate_mesh()
+
+
+func instantiate_mesh() -> void:
 	clearMesh()
+	
+	res.currentLife = res.lifeMax
 	
 	var obj = res.scene.instantiate()
 	mesh_node.add_child(obj)
+	mesh_node.visible = false
 	
-	var collider = Utils.find(mesh_node, PhysicsBody3D)
+	collider = Utils.find(mesh_node, PhysicsBody3D)
 	collider.reparent(self)
+	
+	if res.animateOnInstantiate:
+		anim_player.play(anim_appear)
+	else:
+		mesh_node.visible = true
 
 
 func take_hit(amount :int) -> void:
 	anim_player.stop()
-	anim_player.play(anim_name)
-	res.life -= amount
-	if res.life <= 0:
+	anim_player.play(anim_squash)
+	res.currentLife -= amount
+	if res.currentLife <= 0:
 		selfDestroy()
 
 
 func selfDestroy() -> void:
 	Inventory.add_collectable_list(res.collectable_list)
-	queue_free()
+	if res.isRespawnable:
+		clearMesh()
+		timer.start(res.timeToRespawn)
+	else:
+		queue_free()
+
+
+func _on_respawn_cd_timeout() -> void:
+	instantiate_mesh()
--- scripts/resources/BreakableObjRes.gd
+++ scripts/resources/BreakableObjRes.gd
@@ -1,8 +1,14 @@
 extends Resource
 class_name BreakableObjRes
 
-@export var life: int = 3
+@export var lifeMax: int = 3
+@export var isRespawnable :bool = true
+@export var timeToRespawn :float = 5.0
 
 @export var scene: PackedScene = null
 
 @export var collectable_list :Array[CollectableWithQteRes] = []
+
+@export var animateOnInstantiate :bool = true
+
+var currentLife :int = lifeMax
